87c4981f05ad974dbba91aaf196c9f507017e77f
Matheus Gitirana <matgitirana@gmail.com>
2019-09-05 14:40:25 -0300

    Improved expanded list

--- src/components/DocumentList.tsx
+++ src/components/DocumentList.tsx
@@ -1,73 +1,78 @@
-import React from 'react';
+import React, { ChangeEvent, Component } from 'react';
 import { db } from '../firebase';
 import { collectionData } from 'rxfire/firestore';
 import { startWith } from 'rxjs/operators';
 import {
-  TextField,
-  Button,
-  Typography,
-  ExpansionPanel,
-  ExpansionPanelSummary,
-  ExpansionPanelDetails,
-  Link,
+    Typography,
+    ExpansionPanel,
+    ExpansionPanelSummary,
+    ExpansionPanelDetails,
+    Link,
 } from '@material-ui/core';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
-export default class DocumentList extends React.Component {
-  collection: firebase.firestore.CollectionReference;
-  query: any;
+type State = {
+    documents: any[];
+    expanded: string | false;
+};
 
-  state = {
-    documents: [],
-  };
+export default class DocumentList extends Component<{}, State> {
+    collection: firebase.firestore.CollectionReference;
+    query: any;
 
-  constructor(props: any) {
-    super(props);
+    constructor(props: any) {
+        super(props);
 
-    this.collection = db.collection('documents');
-    this.query = this.collection.orderBy('name');
-  }
+        this.collection = db.collection('documents');
+        this.query = this.collection.orderBy('name');
+    }
 
-  componentDidMount() {
-    collectionData(this.query, 'id')
-      .pipe(startWith([]))
-      .subscribe((u: any) => {
-        this.setState({
-          documents: u,
-        });
-      });
-  }
+    componentDidMount() {
+        collectionData(this.query, 'id')
+            .pipe(startWith([]))
+            .subscribe((u: any) => {
+                this.setState({
+                    documents: u,
+                });
+            });
+    }
 
-  render() {
-    return (
-      <>
-        <TextField label="Name" />
-        <input
-          accept="image/*"
-          style={{ display: 'none' }}
-          id="raised-button-file"
-          type="file"
-        />
-        <label htmlFor="raised-button-file">
-          <Button variant="contained" component="span">
-            Upload
-          </Button>
-        </label>
-        {this.state.documents.map((doc: any) => {
-          return (
-            <ExpansionPanel>
-              <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-                <Typography>{doc.name}</Typography>
-              </ExpansionPanelSummary>
-              <ExpansionPanelDetails>
-                <Typography>
-                  <Link href={doc.link}>Download file</Link>
-                </Typography>
-              </ExpansionPanelDetails>
-            </ExpansionPanel>
-          );
-        })}
-      </>
-    );
-  }
+    handleChange = (panel: string) => {
+        return (event: ChangeEvent<{}>, isExpanded: boolean) => {
+            const expandedPanel = isExpanded ? panel : false;
+            this.setState({
+                expanded: expandedPanel,
+            });
+        };
+    };
+
+    render() {
+        return (
+            <>
+                {this.state &&
+                    this.state.documents.map((doc: any, index: number) => {
+                        const panelId = `panel${index}`;
+                        return (
+                            <ExpansionPanel
+                                expanded={this.state.expanded === panelId}
+                                onChange={this.handleChange(panelId)}
+                            >
+                                <ExpansionPanelSummary
+                                    expandIcon={<ExpandMoreIcon />}
+                                >
+                                    <Typography>{doc.name}</Typography>
+                                </ExpansionPanelSummary>
+                                <ExpansionPanelDetails>
+                                    <Typography>
+                                        <Link href={doc.link}>
+                                            Download file
+                                        </Link>
+                                    </Typography>
+                                </ExpansionPanelDetails>
+                            </ExpansionPanel>
+                        );
+                    })}
+            </>
+        );
+    }
 }
