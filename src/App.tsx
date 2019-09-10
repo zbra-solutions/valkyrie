33f414773df5a341c973e86b03bca208d697ceab
DaniloArruda <da.ni.lo_arruda@outlook.com>
2019-09-10 16:31:34 -0300

    Deleted inline styling to use with classes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
-import { Container, AppBar, Toolbar, IconButton, Button, makeStyles } from '@material-ui/core';
+import {
+    Container,
+    AppBar,
+    Toolbar,
+    IconButton,
+    Button,
+    makeStyles,
+} from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import { withRouter } from 'react-router';
 
@@ -7,32 +14,6 @@ import Routes from './routes';
 import './App.css';
 import { auth } from './firebase';
 import logo from './assets/logo.png';
-
-const useStyles = makeStyles(theme => ({
-    root: {
-      flexGrow: 1,
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
-    },
-    appBar: {
-        background: '#2c465f',
-    },
-    logo: {
-        borderRadius: 4
-    },
-    toolbar: {
-        justifyContent: 'space-between',
-    },
-    appName: {
-        fontSize: '0.9rem',
-        fontWeight: 500,
-        letterSpacing: '0.02857em',
-    }
-}));
 
 function App(props: any) {
     const classes = useStyles();
@@ -49,29 +30,36 @@ function App(props: any) {
 
         if (user) return true;
         else return false;
-    }
+    };
 
     return (
         <>
-            {loggedIn() &&
+            {loggedIn() && (
                 <AppBar position="static" className={classes.appBar}>
                     <Toolbar className={classes.toolbar}>
-                        <div style={{ display: 'flex', alignItems: 'center' }}>
-                            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                        <div className={classes.app}>
+                            <IconButton
+                                edge="start"
+                                className={classes.menuButton}
+                                color="inherit"
+                                aria-label="menu"
+                            >
                                 <MenuIcon />
                             </IconButton>
                             <span className={classes.appName}>VALKYRIE</span>
                         </div>
-                        <img src={logo} alt="Logo" height="32" className={classes.logo} />
-                        <Button
-                            color="inherit"
-                            onClick={signOut}
-                        >
+                        <img
+                            src={logo}
+                            alt="Logo"
+                            height="32"
+                            className={classes.logo}
+                        />
+                        <Button color="inherit" onClick={signOut}>
                             Logout
                         </Button>
                     </Toolbar>
                 </AppBar>
-            }
+            )}
             <main>
                 <Container maxWidth="md">
                     <Routes />
@@ -80,5 +68,35 @@ function App(props: any) {
         </>
     );
 }
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+    appBar: {
+        background: '#2c465f',
+    },
+    logo: {
+        borderRadius: 4,
+    },
+    toolbar: {
+        justifyContent: 'space-between',
+    },
+    app: {
+        display: 'flex',
+        alignItems: 'center',
+    },
+    appName: {
+        fontSize: '0.9rem',
+        fontWeight: 500,
+        letterSpacing: '0.02857em',
+    },
+}));
 
 export default withRouter(App);
--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,9 +1,3 @@
 export var firebaseConfig = {
-    apiKey: "AIzaSyAzVuxtn_60tZu9PV1Md37NkXRs21yYP8k",
-    authDomain: "valkyrie-c5f31.firebaseapp.com",
-    databaseURL: "https://valkyrie-c5f31.firebaseio.com",
-    projectId: "valkyrie-c5f31",
-    storageBucket: "valkyrie-c5f31.appspot.com",
-    messagingSenderId: "259190970120",
-    appId: "1:259190970120:web:e8cb70f0be84eff2"
+    /* Insert firebase config data here */
 };
